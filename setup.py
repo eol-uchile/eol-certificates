7d8bf72a6241e084d7db1cbec7dbb7ca40ee2218
catalinaIlabaca <catalina.a.ilabaca@uchile.cl>
2026-04-28 16:41:04 -0400

    chore: update semantic version to 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="eol_certificates",
-    version="0.0.1",
+    version="0.1.0",
     author="Oficina EOL UChile",
     author_email="eol-ing@uchile.cl",
     description="EOL Certificates",
